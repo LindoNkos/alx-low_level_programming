e0851d0a638bc3de5220e4053bfb9431de36a6bc
lindokuhle <lindokuhlenkosi664@gmail.com>
2022-10-16 09:12:00 -0700

    project 3 ans 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude < stdio.h >
+/**
+ * main-serves as the entry point for the program
+ *
+ * Return: by default returns zero if no errors.
+ *
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
