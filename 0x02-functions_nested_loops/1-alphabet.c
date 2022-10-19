21b6a50bcb51fed522f87db6598a46dfa00e9255
lindokuhle <lindokuhlenkosi664@gmail.com>
2022-10-19 07:25:40 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  main - check the code
+ * description: a function that prints the alphabets,
+ * in lowercase and followed by a new line
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char letter = 'a'
+		while (letter < = 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar("\n");
+
+}
