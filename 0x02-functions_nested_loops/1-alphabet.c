63a54052132e450664a55f0b4cc3689c69d49949
lindokuhle <lindokuhlenkosi664@gmail.com>
2022-10-19 07:40:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,15 @@
  * in lowercase and followed by a new line
  * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char letter = 'a'
-		while (letter < = 'z')
-		{
-			_putchar(letter);
-			letter++;
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
 		}
-	_putchar("\n");
+	_putchar('\n');
 
 }
