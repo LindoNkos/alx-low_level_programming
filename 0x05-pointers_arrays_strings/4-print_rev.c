9b59e6e27e8366b4db239581169fcc98403bf07a
lindokuhle <lindokuhlenkosi664@gmail.com>
2022-10-25 09:22:25 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_rev - prints the string in reverse order
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int  i = 0;
+
+	while (s(i) != '\n')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
