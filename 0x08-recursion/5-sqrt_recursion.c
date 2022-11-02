3c1432811fa620cf2b96a8ca9aa70586d05ee5cf
lindokuhle <lindokuhlenkosi664@gmail.com>
2022-11-02 14:53:38 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _sqr_reecursion - evaluate function sqrt
+ * @i: integer
+ * @n: integer
+ * Return: evaluate sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i == n)
+		return (i);
+
+	return (-1);
+
+	return (-1);
+}
